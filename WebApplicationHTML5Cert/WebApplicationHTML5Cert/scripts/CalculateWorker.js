1ae5f1e6f6f55b8acc5ab98791ba02659e8070c6
tristian2@hotmail.com <808state>
2015-12-31 12:36:50 +0000

    added work processes - these are buggy as hell tho

---
+++ WebApplicationHTML5Cert/WebApplicationHTML5Cert/scripts/CalculateWorker.js
@@ -0,0 +1,11 @@
+﻿onmessage = function (evt) {0    
+    var work = 10000000;
+    var i = 0;
+    var a = new Array(work);
+    var sum = 0;
+    for (i = 0; i < work; i++) {
+        a[i] = i * i;
+        sum += i * i;
+    }
+    self.postMessage(sum);
+}
